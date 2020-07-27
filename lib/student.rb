c6ad12b354a17cb78bb670e8b224b8f6f0d585b2
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-07-27 16:21:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,6 +80,7 @@ class Student
     
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
+    end.first
   end 
   
   def self.all_students_in_grade_x
@@ -91,6 +92,7 @@ class Student
     
     DB[:conn].execute(sql, grade).map do |row|
       self.new_from_db(row)
+    end
   end
   
   def save
